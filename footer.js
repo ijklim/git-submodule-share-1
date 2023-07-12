167232238b9a76f2b5051b0618299f347df18a05
ivan-aim <77643196+ivan-aim@users.noreply.github.com>
2023-07-12 15:33:26 -0400

    Add footer text and styles

--- footer.js
+++ footer.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log('[footer.js] Document is ready');
+
+  const footer = document.createElement('footer');
+  footer.innerHTML = `
+    Brought to you by <a href='https://ivan-lim.com' target='_blank'>Ivan Lim</a>
+  `;
+  document.body.appendChild(footer);
 });
